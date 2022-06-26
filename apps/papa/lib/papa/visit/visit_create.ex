7c3fe8c343231f3f321577d7f8cb4f13fc289579
Taylor Redden <taelor@co-hack.com>
2022-06-26 13:06:41 -0500

    PAPA-4 prevent visit request on zero balance or less

--- apps/papa/lib/papa/visit/visit_create.ex
+++ apps/papa/lib/papa/visit/visit_create.ex
@@ -1,7 +1,14 @@
 defmodule Papa.Visit.Create do
-  alias Papa.Repo
+  alias Papa.{Account, Repo}
 
   def call(user, args) do
+    case Account.get_balance(user.id) > 0 do
+      true -> insert_visit(user, args)
+      false -> {:error, "Member has no minutes left to request visits"}
+    end
+  end
+
+  defp insert_visit(user, args) do
     Ecto.build_assoc(user, :requested_visits, args)
     |> Repo.insert()
   end
--- apps/papa_web/test/papa_web/visit/request_visit_test.exs
+++ apps/papa_web/test/papa_web/visit/request_visit_test.exs
@@ -3,22 +3,53 @@ defmodule PapaWeb.RequestVisitTest do
 
   import Papa.Factory
 
-  setup [:user, :query]
+  alias Papa.Account
 
-  test "request visit", %{conn: conn} = ctx do
-    conn = post(conn, "/api", %{"query" => ctx.query})
+  describe "when user has balance > 0" do
+    setup [:user, :account, :query]
 
-    %{"data" => %{"requestVisit" => visit}} = json_response(conn, 200)
+    test "request visit", %{conn: conn} = ctx do
+      conn = post(conn, "/api", %{"query" => ctx.query})
 
-    assert is_binary(visit["id"])
-    assert visit["tasks"] == "Just hang out"
+      %{"data" => %{"requestVisit" => visit}} = json_response(conn, 200)
+
+      assert is_binary(visit["id"])
+      assert visit["tasks"] == "Just hang out"
+    end
+  end
+
+  describe "when user has 0 or less balance" do
+    setup [:user, :visit, :account, :query]
+
+    test "they cannot request a visit", %{conn: conn} = ctx do
+      conn = post(conn, "/api", %{"query" => ctx.query})
+
+      %{"data" => %{"requestVisit" => nil}, "errors" => errors} = json_response(conn, 200)
+
+      assert Enum.at(errors, 0)["message"] == "Member has no minutes left to request visits"
+    end
   end
 
   # ---------------------------------------------------------------------------
   # Setup
   # ---------------------------------------------------------------------------
 
-  def user(_), do: %{user: insert(:user, email: "test.user@papa.com")}
+  def user(_), do: %{user: insert(:user)}
+
+  def visit(%{user: user}) do
+    %{visit: insert(:visit, member_id: user.id, pal_id: insert(:user).id, minutes: 1000)}
+  end
+
+  def account(%{user: user}) do
+    # At this point, I would refactor this out into a support helper module
+    opts = [user_id: user.id, name: Account.via(user.id)]
+
+    start_supervised({Account, opts}, id: user.id)
+
+    Account.get_balance(user.id)
+
+    :ok
+  end
 
   def query(ctx) do
     query = """
